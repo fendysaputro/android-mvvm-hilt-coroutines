e96e5f4dd826944cc8ae122f313fdc104ebc26c5
fendysaputro <vidis194@gmail.com>
2022-05-30 13:26:26 +0700

    first commit

---
+++ app/src/main/java/id/phephen/mvvmhiltcoroutines/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.phephen.mvvmhiltcoroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
